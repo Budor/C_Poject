fe312a589390a078e60971c2845354add07fce81
smile_zhui <892626731@qq.com>
2017-05-21 18:09:49 +0800

    push hello.c file

--- hello.c
+++ hello.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 #include<time.h>
-typedef unsigned int uint
-typedef unsigned char uchar
-uint cont;
+
 int main(){
 	while(1){
-		sllep(255);
-		printf("hello word c!/n");
+		sleep(255);
+		printf("hello word!");
 	}
 	return 0;
 }
 //新加了一条
-//新加了两条
+//新加了二条
 //新加了三条
 //新加了四条
 //新加了五条
